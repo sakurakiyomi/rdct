17aca0b621bba1ce709732043151efdb70bbf727
sakurakiyomi <moshi2qwq@gmail.com>
2025-04-27 06:08:15 +0700

    Create rdct.js

---
+++ rdct.js
@@ -0,0 +1,12 @@
+if (window.location.pathname == "/" || window.location.pathname == "/index.html") {
+  var lastPage = localStorage.getItem("lastPageVisited");
+  if (lastPage) {
+    window.location.href = lastPage;
+  } else {
+    window.location.href = "/p/hola.html";
+  }
+} else {
+  if (window.location.pathname.startsWith("/p/")) {
+    localStorage.setItem("lastPageVisited", window.location.pathname);
+  }
+}
